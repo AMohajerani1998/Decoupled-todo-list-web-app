59243b1f29bfb7a4184b80234aee516af41ebdb6
AMohajerani1998 <ashkan.mohajerani1998@gmail.com>
2024-05-26 13:39:17 +0200

    Finished task edit in the same place feature

--- scripts/script.js
+++ scripts/script.js
@@ -28,6 +28,7 @@ async function loadTodos() {
         editButton.classList = "edit";
         editButton.textContent = "EDIT";
         editButton.dataset.todoid = todo.id;
+        editButton.addEventListener("click", loadTodoEditForm);
         todoButtonsDiv.appendChild(editButton);
         todoArticle.appendChild(todoTaskH3);
         todoArticle.appendChild(todoButtonsDiv);
@@ -35,29 +36,42 @@ async function loadTodos() {
     }
 }
 
-async function addNewTask(event) {
+async function saveTask(event) {
     event.preventDefault();
-    const formdata = new FormData(event.target.parentElement) 
-    const taskValue = formdata.get('task');
+    const formdata = new FormData(event.target.parentElement);
+    const taskValue = formdata.get("task");
     let response;
     try {
-        response = await fetch("http://localhost:3000/todos", {
-            method: "POST",
-            body: JSON.stringify({
-                task: taskValue
-            }),
-            headers: {
-                "Content-Type": "application/json",
-            }
-        });
+        if (!event.target.dataset.todoid){
+            response = await fetch("http://localhost:3000/todos", {
+                method: "POST",
+                body: JSON.stringify({
+                    task: taskValue,
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+        } else {
+            response = await fetch(`http://localhost:3000/todos/${event.target.dataset.todoid}`, {
+                method: "PATCH",
+                body: JSON.stringify({
+                    task: taskValue,
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+        }
     } catch (error) {
-        alert ('Something went wrong!')
+        alert("Something went wrong!");
     }
-    if (!response.ok){
-        return alert ('Something went wrong')
+    if (!response.ok) {
+        return alert("Something went wrong");
     }
-    const responseData = await response.json()
-    const todo = responseData.createdTodo;
+    const responseData = await response.json();
+    const todo = responseData.todo;
+    const todoDiv = event.target.parentElement.parentElement;
     const todoArticle = document.createElement("article");
     const todoTaskH3 = document.createElement("h3");
     todoTaskH3.textContent = todo.task;
@@ -71,14 +85,41 @@ async function addNewTask(event) {
     editButton.classList = "edit";
     editButton.textContent = "EDIT";
     editButton.dataset.todoid = todo.id;
+    editButton.addEventListener("click", loadTodoEditForm);
     todoButtonsDiv.appendChild(editButton);
     todoArticle.appendChild(todoTaskH3);
     todoArticle.appendChild(todoButtonsDiv);
-    tasksContainer.insertBefore(todoArticle, tasksContainer.firstChild);
+    if (!event.target.dataset.todoid){
+        tasksContainer.insertBefore(todoArticle, tasksContainer.firstChild);
+        return;
+    } else {
+        todoDiv.innerHTML = ''
+        todoDiv.appendChild(todoTaskH3);
+        todoDiv.appendChild(todoButtonsDiv);
+    }
 }
 
-
-
-newTaskSubmitButton.addEventListener("click", addNewTask);
+async function loadTodoEditForm(event) {
+    event.preventDefault;
+    const todoPreviousTask = event.target.parentElement.parentElement.firstChild.innerText
+    const todoId = event.target.dataset.todoid;
+    const todoContainer = event.target.parentElement.parentElement;
+    todoContainer.innerHTML = "";
+    const todoForm = document.createElement("form");
+    todoForm.action = "/todos";
+    todoContainer.appendChild(todoForm);
+    const todoTextArea = document.createElement("textarea");
+    todoTextArea.value = todoPreviousTask
+    todoTextArea.rows = 1;
+    todoTextArea.name = "task";
+    todoForm.appendChild(todoTextArea);
+    const todoSubmitButton = document.createElement("button");
+    todoSubmitButton.textContent = "Save";
+    todoSubmitButton.dataset.todoid = todoId
+    todoForm.appendChild(todoSubmitButton)
+    todoSubmitButton.addEventListener("click", saveTask);
+}
 
 loadTodos();
+
+newTaskSubmitButton.addEventListener("click", saveTask);
